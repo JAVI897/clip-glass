7554347d92eb723c7e3e0992ddd3eb9f3b3db4e5
Javi GG <s.javi.com@gmail.com>
2021-11-22 13:16:55 +0100

    Update pre_trained_transformers_tf.py

--- pre_trained_transformers_tf.py
+++ pre_trained_transformers_tf.py
@@ -3,26 +3,6 @@ from transformer_models.utility import  get_inference_model, generate_caption, c
 import json
 import re
 
-#@tf.keras.utils.register_keras_serializable()
-class custom_schedule(tf.keras.optimizers.schedules.LearningRateSchedule):
-   def __init__(self, d_model, warmup_steps=4000):
-      super(custom_schedule, self).__init__()
-      self.d_model = d_model
-      self.d_model = tf.cast(self.d_model, tf.float32)
-      self.warmup_steps = warmup_steps
-
-   def __call__(self, step):
-      arg1 = tf.math.rsqrt(step)
-      arg2 = step * (self.warmup_steps ** -1.5)
-      return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
-
-   def get_config(self):
-      config = {
-        'd_model': self.d_model,
-        'warmup_steps': self.warmup_steps
-        }
-      return config
-
 models_paths = {'EfficientNetB0 pre-trained on imagenet': 
 					{512:['transformer_models/EfficientNetB0_imagenet_model_1/tokenizer', 
 					      'transformer_models/EfficientNetB0_imagenet_model_1/config_train.json', 
