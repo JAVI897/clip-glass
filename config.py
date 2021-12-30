d08632dea96c0776f3ea0c3bb24974bd6680ba89
Javi GG <s.javi.com@gmail.com>
2021-12-30 22:11:03 +0100

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from utils import biggan_norm, biggan_denorm
 configs = dict(
     GPT2 = dict(
         task = "img2txt",
-        dim_z = 30,
+        dim_z = 20,
         max_tokens_len = 30,
         max_text_len = 50,
         encoder_size = 50257,
@@ -21,9 +21,9 @@ configs = dict(
         pop_size = 100,
         batch_size = 25,
         problem_args = dict(
-            n_var = 30,
+            n_var = 20,
             n_obj = 1,
-            n_constr = 30,
+            n_constr = 20,
             xl = 0,
             xu = 50256
         )
